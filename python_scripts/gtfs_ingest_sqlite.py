9f8910b162b8b24a171d54c6c156c97eb6703efe
aobergdc <aobergdc@gmail.com>
2016-09-15 11:08:50 -0400

    Create gtfs_ingest_sqlite.py

---
+++ python_scripts/gtfs_ingest_sqlite.py
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------
+# Name:        GTFS Ingest
+# Purpose: To ingest a GTFS feed into a sqlite file
+#
+# Author:      Alex Oberg
+#
+# Created:     9/12/2016
+
+#-------------------------------------------------------------------------------
+
+import datetime
+import pygtfs
+
+start_time = datetime.datetime.now()
+print('\nStart at ' + str(start_time))
+
+# CONFIG
+# Update the following paths as needed
+gtfs_feed= r"C:\tasks\2016_09_12_GTFS_ingest\MBTA\MBTA_GTFS.zip"
+output_sqlite = r"C:\tasks\2016_09_12_GTFS_ingest\MBTA\mbta_gtfs.sqlite"
+
+# MAIN
+
+#Create blank sqlite file
+sched = pygtfs.Schedule(output_sqlite)
+
+#Ingest GTFS feed into sqlite file
+pygtfs.append_feed(sched, gtfs_feed)
+
+print ("Done creating sqlite file")
+
+end_time = datetime.datetime.now()
+total_time = end_time - start_time
+print ("\nEnd at {}.  Total run time {}".format(end_time, total_time))
