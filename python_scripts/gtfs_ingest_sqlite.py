4d2329f22de8a3ce20dcf0d1ec51ddbab5a0bcde
aobergdc <aobergdc@gmail.com>
2016-09-15 16:46:59 -0400

    Added documentation
    Info on setting up pygtfs module

--- python_scripts/gtfs_ingest_sqlite.py
+++ python_scripts/gtfs_ingest_sqlite.py
@@ -6,6 +6,13 @@
 #
 # Created:     9/12/2016
 
+# Script requries the pygtfs Python module.
+
+# How to install pygtfs:
+# Run the following in command line (Assume pip is already installed, you may need to modify Python path)
+# C:\Python27\ArcGIS10.3\python.exe -m pip install -U pygtfs
+
+
 #-------------------------------------------------------------------------------
 
 import datetime
